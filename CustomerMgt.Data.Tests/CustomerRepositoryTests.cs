907f3203666ff41500cd3267058ee372ee8d44c6
joezelle <51567424+joezelle@users.noreply.github.com>
2024-07-14 14:27:14 +0100

    Other Data Layer test

--- CustomerMgt.Data.Tests/CustomerRepositoryTests.cs
+++ CustomerMgt.Data.Tests/CustomerRepositoryTests.cs
@@ -34,21 +34,86 @@ namespace CustomerMgt.Data.Tests
             };
         }
 
+        private async Task<CustomerModel> CreateAndSaveCustomerAsync(CustomerModel customer)
+        {
+            var createdCustomer = await _repositoryTests.Create(customer);
+            await _appContext.SaveChangesAsync();
+            return createdCustomer;
+        }
+
+       
+
         [Fact]
         public async Task AddCustomer_ValidCustomer_SavesToDatabase()
         {
 
             var mockCustomer = GetMockCustomer();
 
-            var customer = await _repositoryTests.Create(mockCustomer);
-            customer.FirstName.Should().Be("John");
-            customer.Should().NotBeNull();
+            var newCustomer = await CreateAndSaveCustomerAsync(mockCustomer);
+            newCustomer.FirstName.Should().Be("John");
+            newCustomer.Should().NotBeNull();
 
-            var savedCustomer = await _appContext.Customers.FindAsync(customer.Id);
+            var savedCustomer = await _appContext.Customers.FindAsync(newCustomer.Id);
             savedCustomer.Should().NotBeNull();
             savedCustomer?.FirstName.Should().Be("John");
             savedCustomer?.LastName.Should().Be("Doe");
             
+        }
+
+        [Fact]
+        public async Task EditCustomer_ValidCustomer_SavesToDatabase()
+        {
+            var mockCustomer = GetMockCustomer();
+            var newCustomer = await CreateAndSaveCustomerAsync(mockCustomer);
+
+            // Modify the customer details
+            newCustomer.FirstName = "Jane";
+            newCustomer.LastName = "Smith";
+            newCustomer.Gender = "Female";
+            newCustomer.Address = "456 Main St";
+            newCustomer.Email = "jane.smith@email.com";
+
+
+            var updatedCustomer = await _repositoryTests.Update(newCustomer);
+
+            updatedCustomer.FirstName.Should().Be("Jane");
+            updatedCustomer.Should().NotBeNull();
+
+
+            var savedCustomer = await _appContext.Customers.FindAsync(updatedCustomer.Id);
+
+            savedCustomer.Should().NotBeNull();
+            savedCustomer?.FirstName.Should().Be("Jane");
+            savedCustomer?.LastName.Should().Be("Smith");
+            savedCustomer?.Email.Should().Be("jane.smith@email.com");
+        }
+
+
+        [Fact]
+        public async Task DeleteCustomer_ById_SavesToDatabase()
+        {
+            var mockCustomer = GetMockCustomer();
+            var newcustomer = await CreateAndSaveCustomerAsync(mockCustomer);
+
+
+            var deletedCustomer = await _repositoryTests.Delete(newcustomer.Id);
+
+            deletedCustomer.Should().NotBeNull();
+            deletedCustomer.IsDeleted.Should().BeTrue();
+            deletedCustomer.IsActive.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task GetCustomer_ById_ReturnsCustomer()
+        {
+            var mockCustomer = GetMockCustomer();
+            var newCustomer = await CreateAndSaveCustomerAsync(mockCustomer);
+
+            var retrievedCustomer = await _repositoryTests.Get(newCustomer.Id);
+            retrievedCustomer.Should().NotBeNull();
+            retrievedCustomer.Id.Should().Be(newCustomer.Id);
+
+
         }
     }
 }
--- CustomerMgt.Data/Repositories/CustomerRepository.cs
+++ CustomerMgt.Data/Repositories/CustomerRepository.cs
@@ -28,5 +28,54 @@ namespace CustomerMgt.Data.Repositories
             return customer.Map();
 
         }
+
+        public async Task<CustomerModel> Update(CustomerModel model)
+        {
+            var entity = await _dbContext.Set<Customer>().FindAsync(model.Id);
+            if (entity == null)
+                return null;
+
+            entity.Address = model.Address;
+            entity.Gender = model.Gender;
+            entity.FirstName = model.FirstName;
+            entity.LastName = model.LastName;
+            entity.Email = model.Email;
+
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity.Map();
+        }
+
+        public async Task<CustomerModel> Delete(long Id)
+        {
+            var entity = await _dbContext.Set<Customer>().FindAsync(Id);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            if (!entity.IsDeleted)
+            {
+                entity.IsActive = false;
+                entity.IsDeleted = true;
+                await _dbContext.SaveChangesAsync();
+            }
+                
+
+            return entity.Map();
+        }
+
+        public async Task<CustomerModel> Get(long id)
+        {
+            var entity = await _dbContext.Set<Customer>().FindAsync(id);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return entity.Map();
+        }
+
     }
 }
